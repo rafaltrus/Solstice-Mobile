9030bb308c65f211486de440ba6cdd658be45d61
Rafal Truszkowski <truszko1@gmail.com>
2015-06-03 01:54:31 -0500

    added phone section

--- app/src/main/java/com/truszkowski/rafal/contacts/app/ContactDetailFragment.java
+++ app/src/main/java/com/truszkowski/rafal/contacts/app/ContactDetailFragment.java
@@ -10,12 +10,14 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import org.json.JSONException;
+import org.json.JSONObject;
 
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLConnection;
 
 /**
  * A fragment representing a single Contact detail screen.
@@ -29,6 +31,7 @@ public class ContactDetailFragment extends Fragment {
      * represents.
      */
     public static final String CONTACT_NAME = "item_id";
+
 
     /**
      * The dummy content this fragment is presenting.
@@ -61,8 +64,16 @@ public class ContactDetailFragment extends Fragment {
         // Show the dummy content as text in a TextView.
         if (mContact != null) {
             new GetContactImage().execute();
+            new GetMoreContactDetails().execute(mContact.detailsURL);
             ((TextView) rootView.findViewById(R.id.name)).setText(mContact.name);
             ((TextView) rootView.findViewById(R.id.company)).setText(mContact.company);
+            try {
+                ((TextView) rootView.findViewById(R.id.workPhone)).setText(mContact.phone.getString("work"));
+                ((TextView) rootView.findViewById(R.id.homePhone)).setText(mContact.phone.getString("home"));
+                ((TextView) rootView.findViewById(R.id.mobilePhone)).setText(mContact.phone.getString("mobile"));
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
         }
         return rootView;
     }
@@ -96,6 +107,53 @@ public class ContactDetailFragment extends Fragment {
             if (bitmap != null) {
                 ((ImageView) getActivity().findViewById(R.id.image)).setImageBitmap(bitmap);
             }
+        }
+    }
+
+    class GetMoreContactDetails extends AsyncTask<String, Void, JSONObject> {
+
+        @Override
+        protected JSONObject doInBackground(String... strings) {
+            // AndroidHttpClient class is deprecated in API level 22. It is advised to use URLConnection instead.
+            // ref: http://developer.android.com/reference/android/net/http/AndroidHttpClient.html
+            URL url = null;
+            try {
+                url = new URL(strings[0]);
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            }
+            URLConnection urlConnection;
+            InputStream in = null;
+            try {
+                assert url != null;
+                urlConnection = url.openConnection();
+                in = new BufferedInputStream(urlConnection.getInputStream());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            assert in != null;
+            BufferedReader r = new BufferedReader(new InputStreamReader(in));
+            StringBuilder total = new StringBuilder();
+            String line;
+            try {
+                while ((line = r.readLine()) != null) {
+                    total.append(line);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            try {
+                return new JSONObject(total.toString());
+            } catch (JSONException e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(JSONObject jsonContactDetails) {
+            super.onPostExecute(jsonContactDetails);
         }
     }
 }
